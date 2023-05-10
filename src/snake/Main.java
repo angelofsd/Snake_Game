0fe8602f57389fa4bf11158884289143c2bfd2e8
angelofsd <angelsec@protonmail.com>
2023-05-10 16:27:07 -0600

    Added collision with self ending game.

--- src/snake/Main.java
+++ src/snake/Main.java
@@ -1,5 +1,17 @@
+package snake;
+
+import javax.swing.*;
+
+//testing the update
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Snake Game");
+        SnakeGame snakeGame = new SnakeGame();
+        snakeGame.init();
+        frame.add(snakeGame);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
